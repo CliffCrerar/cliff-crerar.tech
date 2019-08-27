f07fbb61d22e62c863da11444bf6d0eec66d1ab5
Cliff Crerar <cliff.crerar@gmail.com>
2019-08-27 02:51:43 +0200

    updated

--- src/app.ts
+++ src/app.ts
@@ -23,33 +23,8 @@ const {header,main,footer,bgMatter} = (() =>{
 bgMatter.style.position = 'absolute';
 bgMatter.style.minHeight = '100vh';
 bgMatter.style.width = '100vw';
-
 bgMatter.appendChild(videobg());
 bgMatter.appendChild(splitview());
 header.appendChild(nav);
 main.appendChild(content());
 footer.innerHTML = policies();
-console.log('content: ', content);
-
-
-// document.body.appendChild(videobg());
-// document.body.appendChild(splitview());
-// app.appendChild(content());
-
-
-
-
-// !process.env.DEV && googleScr();
-// function googleScr() {
-//     var gscr = document.querySelector('#googleScr');
-//     gscr.setAttribute('type', 'text/javascript');
-//     console.log('gscr: ', gscr);
-//     const src = `
-//     window.dataLayer = window.dataLayer || [];
-//     function gtag() {
-//         dataLayer.push(arguments);
-//     }
-//     gtag('js', new Date());
-//     gtag('config', 'UA-143913069-1');`
-//     gscr.text = src;
-// }
