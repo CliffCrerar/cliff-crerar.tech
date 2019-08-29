ac54e0ac0b4c82206b236c9818809847a9666832
Cliff Crerar <cliff.crerar@gmail.com>
2019-08-29 23:17:22 +0200

    Privacy and terms

--- src/footer/index.ts
+++ src/footer/index.ts
@@ -7,9 +7,9 @@ export default () => {
     return `
         <div class="footer">
             
-        <span><a class="policies" href="${privacy}">Privacy Policy</a></span>
+        <span><a class="policies" href="${privacy}" target="_blank">Privacy Policy</a></span>
         <span><|></span>
-        <span><a class="policies" href="${terms}">Terms of use</a></span>
+        <span><a class="policies" href="${terms}" target="_blank">Terms of use</a></span>
         
         </div>
     `
