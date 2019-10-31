166110e4f161a532138b4b81d3f9a43e1c1e9edd
Cliff Crerar <cliff.crerar@gmail.com>
2019-10-31 06:07:20 +0200

    Moved elements into a grid format, looks much more organised

--- src/_page-content/calendly/calendly.ts
+++ src/_page-content/calendly/calendly.ts
@@ -1,11 +1,28 @@
 export default `
+    <style>
+        p{
+            float: center;
+        }
+        a:hover p,a:hover img{
+            transform: scale(1.2);
+        }
+    </style>
     <div class="calendly-box">
-        <a href="https://calendly.com/cliff-crerar/60min" target="blank">
+    <button>
+        <a href="https://calendly.com/cliff-crerar/session-with-dev" target="blank">
             <img 
             style="height: 100px; width: 100px" 
             src="${require('../../_assets/calendly-1.png')}" 
             alt="calendly">
+            <p style="font-size: 1.5em">Make an appointment</p>
         </a>
-        <p style="font-size: 1.5em">Arrange a meeting not with Calendly</p>
+    </button>
     </div>
 `
+
+/**
+ * <!-- Calendly inline widget begin -->
+<div class="calendly-inline-widget" data-url="https://calendly.com/cliff-crerar/session-with-dev" style="min-width:320px;height:630px;"></div>
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
+<!-- Calendly inline widget end -->
+ */
