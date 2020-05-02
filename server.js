4974098ea83146e142aa7364e6cb663e46234f2d
C1I7F05D-C53545 <cliff.crerar@gmail.com>
2020-05-02 20:54:55 +0000

    added drift

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const $ = require;
-const middleware = $('webpack-dev-middleware');
-const config = $('./webpack.config.js');
-const webpack = $('webpack');
-const express = $('express');
-const compiler = webpack(config);
-const frontEnd = middleware(compiler);
-const app = express().use(frontEnd).listen(3000);
+const 
+    $ = require,
+    middleware = $('webpack-dev-middleware'),
+    config = $('./webpack.config.js'),
+    webpack = $('webpack'),
+    express = $('express'),
+    compiler = webpack(config),
+    frontEnd = middleware(compiler),
+    app = express().use(frontEnd).listen(3000);
