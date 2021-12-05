e181eba24319a721a346c5272ec9b0428e0543c8
Cliff <cliff@infinityarc.nete>
2021-12-05 18:20:44 +0200

    cliff.crerar.tech

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders without crashing', () => {
-  const { baseElement } = render(<App />);
-  expect(baseElement).toBeDefined();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,26 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import './App.css';
 
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {Home} from "./pages";
 
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
 
-/* Theme variables */
-import './theme/variables.css';
+// import { Switch } from '@mui/material';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+function App() {
+  return (
+   <Router>
+     <nav>
+        NAV
+     </nav>
+     <Switch>
+       <Route path="/">
+         <Home/>
+
+       </Route>
+     </Switch>
+   </Router>
+  );
+}
 
 export default App;
